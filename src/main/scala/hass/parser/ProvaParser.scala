36b26849b8b489a7a6243148e6eeca0fe722fec2
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2020-05-01 14:03:54 +0200

    Some trials with monadic parser

--- src/main/scala/hass/parser/ProvaParser.scala
+++ src/main/scala/hass/parser/ProvaParser.scala
@@ -4,6 +4,14 @@ object ProvaParser extends App {
 
   implicit def lol[O](p: Parser[Seq[Char], O]): StrParser[O] = StrParser(p)
 
+  implicit def asd(c: Char): StrParser[Char] = str(c)
+
+  implicit def asd2(s: String): StrParser[String] = str(s)
+
+  object Parser {
+    def unit[I, O](out: O): Parser[I, O] = ParserImpl(i => Seq((out, i)))
+  }
+
   trait Parser[I, +O] extends (I => Seq[(O, I)]) {
     def run: I => Seq[(O, I)]
 
@@ -20,7 +28,7 @@ object ProvaParser extends App {
     def filter(f: O => Boolean): Parser[I, O] =
       ParserImpl(input => this (input).filter { case (o, _) => f(o) })
 
-    def takeWhile(f: O => Boolean):Parser[I, Seq[O]] = many(this).filter(s => s.forall(f))
+    def takeWhile(f: O => Boolean): Parser[I, Seq[O]] = this.*.filter(s => s.forall(f))
 
     def collect[T](f: PartialFunction[O, T]): Parser[I, T] = filter(f.isDefinedAt).map(f)
 
@@ -36,6 +44,13 @@ object ProvaParser extends App {
 
     def <~[T](p2: => Parser[I, T]): Parser[I, O] = (this ~ p2).map(_._1)
 
+    def ? : Parser[I, Option[O]] = (for (v <- this) yield Some(v)) | Parser.unit(None)
+
+    def * : Parser[I, Seq[O]] =
+      (for (out <- this; rest <- this.*) yield out +: rest) | Parser.unit(Seq())
+
+    def + : Parser[I, Seq[O]] =
+      for (out <- this; rest <- this.*) yield out +: rest
   }
 
   case class ParserImpl[I, +O](run: I => Seq[(O, I)]) extends Parser[I, O]
@@ -43,71 +58,52 @@ object ProvaParser extends App {
   case class StrParser[+O](run: Seq[Char] => Seq[(O, Seq[Char])]) extends Parser[Seq[Char], O]
 
 
-  val asd = BigDecimal.apply("23452345.23452345")
-
-  /*def many[I,O](p:Parser[I,O]):Parser[I,Seq[O]] = s => {
-    var str = s
-    while(p(str).nonEmpty) {
-
-    }
-  }*/
-
-  def many[I, O](p: Parser[I, O]): Parser[I, Seq[O]] = {
-    type A = Seq[O] //intellij is bugged
-    val empty: Parser[I, A] = ParserImpl(i => Seq((Seq(), i)))
-    val parser: Parser[I, A] = p.flatMap(out => many(p).map(rest => Seq(out) ++ rest))
-    empty | parser
-  }
-
-  def maybe[I, O](p: Parser[I, O]): Parser[I, Option[O]] = many(p).map(_.headOption)
-
-  def next: StrParser[Char] =
-    StrParser(s => s.headOption.map(h => LazyList((h, s.tail))).getOrElse(Nil))
+  def next: StrParser[Char] = StrParser(s => s.headOption.map(h => LazyList((h, s.tail))).getOrElse(Nil))
 
   def str(c: Char): StrParser[Char] = next.filter(_ == c)
 
-  def str(s: Seq[Char]): StrParser[Seq[Char]] = StrParser(input => input.take(s.length) match {
-    case v if v equals s => LazyList((s, input.drop(s.length)))
+  def str(s: String): StrParser[String] = next.*.collect { case seq if seq.mkString == s => s}
+  /*StrParser(input => input.take(s.length) match {
+    case v if v equals s.toSeq => LazyList((s, input.drop(s.length)))
     case _ => Nil
-  })
+  })*/
 
   def digit: StrParser[Char] = next.filter(_.isDigit)
 
-  def digits: StrParser[String] = many(digit).map(_.mkString).filter(_.nonEmpty)
+  def digits: StrParser[String] = digit.+.map(_.mkString)
 
-  def integer: StrParser[BigDecimal] = {
-    digits.collect {
-      case seq if seq.nonEmpty => BigDecimal(seq.mkString)
-    }
-  }
+  def integer: StrParser[Long] = digits.map(_.toLong)
 
-  def decimal: StrParser[BigDecimal] =
-    (integer ~ maybe(str('.') ~> digits)) map {
-      case (int, Some(dec)) => int + BigDecimal("0." + dec)
+  def decimal: StrParser[Double] =
+    integer ~ ('.' ~> digits).? map {
+      case (int, Some(dec)) => int + ("0." + dec).toDouble
       case (int, None) => int
     }
 
-  def expr: StrParser[BigDecimal] =
-    term ~ many(str('+') ~ term | str('-') ~ term) map {
-      case (decimal, seq) => seq.foldLeft(decimal) {
-        case (value, ('+', n)) => value + n
-        case (value, ('-', n)) => value - n
-      }
-    }
+  def expr: StrParser[Double] =
+    term ~ ('+' ~ term | '-' ~ term).* map calc
 
-  def term: StrParser[BigDecimal] =
-    factor ~ many(str('*') ~ factor | str('/') ~ factor) map {
-      case (decimal, seq) => seq.foldLeft(decimal) {
-        case (value, ('*', n)) => value * n
-        case (value, ('/', n)) => value / n
-      }
-    }
+  def term: StrParser[Double] =
+    power ~ ('*' ~ power | '/' ~ power).* map calc
 
-  def factor: StrParser[BigDecimal] = decimal | (str('(') ~> expr <~ str(')'))
+  def power: StrParser[Double] =
+    factor ~ ('^' ~ factor ).* map calc
 
-  println(next.takeWhile(_ != ';').map(_.mkString)("234asd;456").toList)
+  def factor: StrParser[Double] = decimal | ('(' ~> expr <~ ')')
 
-  println(expr("(10+(20.5*30)/40-7)*10000").collectFirst {
+  println(digits ("1234a34").toList)
+
+  println(expr("3^3*2+1").collectFirst {
     case (a, Nil) => a
   })
+
+  def calc:((Double, Seq[(Char, Double)])) => Double = {
+    case (decimal, seq) => seq.foldLeft(decimal) {
+      case (value, ('*', n)) => value * n
+      case (value, ('/', n)) => value / n
+      case (value, ('+', n)) => value + n
+      case (value, ('-', n)) => value - n
+      case (value, ('^', n)) => Math.pow(value, n)
+    }
+  }
 }
